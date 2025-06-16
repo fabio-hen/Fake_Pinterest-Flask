e4875d7476b73e0e49953fe40fdce75139c2bc8d
fabio-hen <113725177+fabio-hen@users.noreply.github.com>
2025-06-16 15:17:57 -0300

    primeiro commit

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fakepinterest import app
 
-if __name__ == "__main__":  # se esse arquivo esta executando ele mesmo
-    app.run(debug=True)
+
+if __name__ == "__main__":
+    app.run(debug=False)
